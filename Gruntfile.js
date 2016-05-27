0befd13c8c7776c9531ce23dc34de443a14d474f
gjh <guy@swirrl.com>
2016-05-27 15:17:25 +0100

    pack media queries

--- Gruntfile.js
+++ Gruntfile.js
@@ -55,6 +55,7 @@ module.exports = function(grunt) {
 			options: {
 				map: false,
 				processors: [
+					require('css-mqpacker')(),
 					require('pixrem')(), // add fallbacks for rem units
 					require('autoprefixer')({browsers: 'last 2 versions'}), // add vendor prefixes
 					// require('cssnano')() // minify the result
